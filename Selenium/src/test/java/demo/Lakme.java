38bf68bb282b53acf2cb6cd143e0896efc53cd5f
satis <satis@SatishPC>
2024-05-31 15:34:11 +0530

    New class "Lakme" is added in demo package.

---
+++ Selenium/src/test/java/demo/Lakme.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Lakme {
+    @Test
+	public void testCase01() {
+		
+		Reporter.log("This is a Lakme Application for beauty", true);
+	}
+}
